b3b6775563b7eb804b0733381c0b6d104062e230
Mathieu Dutour <mathieu@dutour.me>
2016-08-27 12:49:45 +0100

    add .sketchignore file

--- Git.sketchplugin/exportArtboard.sh
+++ Git.sketchplugin/exportArtboard.sh
@@ -15,9 +15,23 @@ mkdir -p "$EXPORT_FOLDER"
 rm -rf .oldArtboards
 mv "$FILE_FOLDER" .oldArtboards 2>/dev/null
 
+# get list of artboards regex to ignore
+IGNORE=$([ -e .sketchignore ] && (cat .sketchignore | sed '/^$/d' | sed 's/^/^/' | sed 's/$/$/' | tr '\n' ',') || echo "")
+
+# get list of artboard names to export
+ARTBOARDS=$($BUNDLE_PATH/Contents/Resources/sketchtool/bin/sketchtool list artboards "$FILENAME" | grep -Eo '"name" :.*?[^\\]",' | cut -d '"' -f 4 | awk '{ N = split("'"$IGNORE"'", ignore, ",");
+  for (i=1; i<=N; i++)
+    if (ignore[i] && $0 ~ ignore[i]) {
+      break
+    } else if (i == N) {
+      print $0
+    }
+}' | tr '\n' ',')
+
+
 # generate new artboards
 mkdir -p "$FILE_FOLDER"
-$BUNDLE_PATH/Contents/Resources/sketchtool/bin/sketchtool export artboards "$FILENAME" --scales="$SCALE" --output="$FILE_FOLDER" --overwriting=YES
+$BUNDLE_PATH/Contents/Resources/sketchtool/bin/sketchtool export artboards "$FILENAME" --scales="$SCALE" --output="$FILE_FOLDER" --overwriting=YES --items="$ARTBOARDS"
 
 # compare new artboards with the old ones
 for artboardPath in "$FILE_FOLDER"/*
