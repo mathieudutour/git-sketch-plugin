b009fd84237e8aabe37fc6ab6c4281b08a379a2d
tomonari-t <tomo1208.japan@gmail.com>
2016-09-30 22:10:54 +0900

    fixed unicode and encode Error for artboard

--- Git.sketchplugin/getArtboardNames.py
+++ Git.sketchplugin/getArtboardNames.py
@@ -8,6 +8,10 @@ ignore = map(re.compile, filter(None, sys.argv[1].split(",")))
 
 for page in obj["pages"]:
     for artboard in page["artboards"]:
-        name = page["name"] + "/" + artboard["name"]
+        # For Unicode error
+        pageName = page["name"].encode("utf-8")
+        artboardName = artboard["name"].encode("utf-8")
+
+        name = pageName + "/" + artboardName
         if len(ignore) == 0 or all(regex.match(name) == None for regex in ignore):
-            print artboard["name"]
+            print(artboardName)
